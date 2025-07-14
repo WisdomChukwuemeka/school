434b41cc9874308663e97763232326816a992784
wisdom chukwuemeka <wisdomchukwuemeka97@gmail.com>
2025-07-14 15:05:29 +0100

    code build modification

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/school"
+  base: "/school",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
 })
